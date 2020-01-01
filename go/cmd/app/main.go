3c9abcd64768ef2e6bd86307d7fce9814c3e6694
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-01-01 16:54:45 -0500

    Cleanup main re: changes to pipanel interfaces

--- go/cmd/app/main.go
+++ go/cmd/app/main.go
@@ -9,14 +9,10 @@ import (
 
 	pipanel "github.com/BenJetson/pipanel/go"
 	"github.com/BenJetson/pipanel/go/frontends"
-	"github.com/BenJetson/pipanel/go/middleware/errorlog"
 	"github.com/BenJetson/pipanel/go/server"
-	"github.com/gotk3/gotk3/gtk"
 )
 
 func main() {
-	fmt.Printf("AACK!")
-
 	// Create log instances.
 	logServer := log.New(os.Stdout, "server", log.LstdFlags)
 	logFrontend := log.New(os.Stdout, "frontend", log.LstdFlags)
@@ -29,15 +25,9 @@ func main() {
 	signal.Notify(interrupt, os.Interrupt)
 
 	// Create the frontend instance.
-	gtk.Init(nil)
-	go gtk.Main()
-
-	var frontend pipanel.Frontend
+	var frontend *pipanel.Frontend
 	// frontend = frontends.NewConsoleFrontend(logFrontend)
 	frontend = frontends.NewPiPanelGTK(logFrontend)
-
-	// Use error log middleware.
-	frontend = errorlog.New(frontend, logFrontend)
 
 	// Start the server.
 	server := server.New(logServer, 1035, frontend)
@@ -49,11 +39,8 @@ func main() {
 	case <-interrupt:
 		fmt.Println("sigint detected")
 		server.Shutdown(context.TODO()) // nolint: errcheck
-		gtk.MainQuit()
 	case <-shutdown:
 		fmt.Println("server shutdown detected")
 		server.Shutdown(context.TODO()) // nolint: errcheck
-		gtk.MainQuit()
 	}
-
 }
