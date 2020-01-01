f88549fd74a9ddcf2a8887fc05b8bfed93d81cae
Ben Godfrey <BenJetson@users.noreply.github.com>
2019-12-31 19:46:10 -0500

    Create systemd power manager.

---
+++ go/frontends/power_managers/systemd/systemd.go
@@ -0,0 +1,38 @@
+package systemdpwr
+
+import (
+	"log"
+	"os/exec"
+
+	pipanel "github.com/BenJetson/pipanel/go"
+)
+
+var shutdownCmd *exec.Cmd = exec.Command("shutdown", "now")
+var rebootCmd *exec.Cmd = exec.Command("reboot", "now")
+var displayOffCmd *exec.Cmd = exec.Command("xset dpms force off")
+
+// SystemdPowerManager handles pipanel power events for systemd-based systems
+// with X display servers.
+type SystemdPowerManager struct {
+	log *log.Logger
+}
+
+// New creates a SystemdPowerManager instance.
+func New(log *log.Logger) *SystemdPowerManager {
+	return &SystemdPowerManager{log}
+}
+
+// DoPowerAction handles pipanel power events.
+func (s *SystemdPowerManager) DoPowerAction(e pipanel.PowerEvent) error {
+	switch e.Action {
+	case pipanel.PowerActionShutdown:
+		s.log.Println("Shutting down the system NOW.")
+		return shutdownCmd.Run()
+	case pipanel.PowerActionReboot:
+		s.log.Println("Rebooting the system NOW.")
+		return rebootCmd.Run()
+	case pipanel.PowerActionDisplayOff:
+		s.log.Println("Turning off the display.")
+		return displayOffCmd.Run()
+	}
+}
