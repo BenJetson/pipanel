0ce1d2093e80993d5af8589b52bf5f33d53299a0
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-01-01 14:46:42 -0500

    Rework PiPanel interfaces to support init/cleanup methods.

--- go/pipanel.go
+++ go/pipanel.go
@@ -2,36 +2,98 @@ package pipanel
 
 import "time"
 
+// InitCleaner allows PiPanel components to perform actions prior to their first
+// usage or the final teardown.
+type InitCleaner interface {
+	// Init performs any necessary setup that needs to be done prior to starting
+	// a PiPanel component.
+	Init() error
+	// Cleanup performs any necessary cleanup and teardown that needs to be done
+	// prior to halting a PiPanel component.
+	Cleanup() error
+}
+
 // An Alerter presents alert messages to the user.
 type Alerter interface {
+	InitCleaner
 	// ShowAlert displays an alert on the screen.
 	ShowAlert(e AlertEvent) error
 }
 
 // An AudioPlayer plays audio clips stored on the system.
 type AudioPlayer interface {
+	InitCleaner
 	// PlaySound plays the specified sound.
 	PlaySound(e SoundEvent) error
 }
 
 // A PowerManager controls system power functions.
 type PowerManager interface {
+	InitCleaner
 	// DoPowerAction performs the system power action.
 	DoPowerAction(e PowerEvent) error
 }
 
 // A DisplayManager controls properties of the display.
 type DisplayManager interface {
+	InitCleaner
 	// SetBrightness alters the brightness of the panel.
 	SetBrightness(e BrightnessEvent) error
 }
 
 // Frontend provides abstraction for processing PiPanel events.
-type Frontend interface {
+type Frontend struct {
+	InitCleaner
 	Alerter
 	AudioPlayer
 	PowerManager
 	DisplayManager
+}
+
+// Init initializes all components of the Frontend.
+func (f *Frontend) Init() {
+	if f.InitCleaner != nil {
+		f.InitCleaner.Init()
+	}
+
+	if f.Alerter != nil {
+		f.Alerter.Init()
+	}
+
+	if f.AudioPlayer != nil {
+		f.AudioPlayer.Init()
+	}
+
+	if f.PowerManager != nil {
+		f.PowerManager.Init()
+	}
+
+	if f.DisplayManager != nil {
+		f.DisplayManager.Init()
+	}
+}
+
+// Cleanup tears down all components of the Frontend.
+func (f *Frontend) Cleanup() {
+	if f.InitCleaner != nil {
+		f.InitCleaner.Cleanup()
+	}
+
+	if f.Alerter != nil {
+		f.Alerter.Cleanup()
+	}
+
+	if f.AudioPlayer != nil {
+		f.AudioPlayer.Cleanup()
+	}
+
+	if f.PowerManager != nil {
+		f.PowerManager.Cleanup()
+	}
+
+	if f.DisplayManager != nil {
+		f.DisplayManager.Cleanup()
+	}
 }
 
 // An AlertEvent contains information about an alert display request.
