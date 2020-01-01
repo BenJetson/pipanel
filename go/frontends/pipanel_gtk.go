b1d23d151db5a6312100a97dc1636ada14d3c753
Ben Godfrey <BenJetson@users.noreply.github.com>
2019-12-31 22:54:29 -0500

    Make PiPanelGTK both display and read alerts.

--- go/frontends/pipanel_gtk.go
+++ go/frontends/pipanel_gtk.go
@@ -3,6 +3,9 @@ package frontends
 import (
 	"log"
 
+	pipanel "github.com/BenJetson/pipanel/go"
+	"github.com/BenJetson/pipanel/go/frontends/alerters/ttsalerter"
+
 	"github.com/BenJetson/pipanel/go/frontends/alerters/gtkalerter"
 	"github.com/BenJetson/pipanel/go/frontends/audio_players/audiolog"
 	"github.com/BenJetson/pipanel/go/frontends/display_managers/displaylog"
@@ -12,6 +15,7 @@ import (
 type PiPanelGTK struct {
 	*audiolog.AudioLog
 	*gtkalerter.GUI
+	*ttsalerter.TTSAlerter
 	*displaylog.DisplayLog
 	*powerlog.PowerLog
 }
@@ -19,8 +23,20 @@ type PiPanelGTK struct {
 func NewPiPanelGTK(log *log.Logger) *PiPanelGTK {
 	return &PiPanelGTK{
 		GUI:        gtkalerter.New(log),
+		TTSAlerter: ttsalerter.New(log, "/tmp/alert-tts/", "en"),
 		AudioLog:   audiolog.New(log),
 		DisplayLog: displaylog.New(log),
 		PowerLog:   powerlog.New(log),
 	}
+}
+
+// ShowAlert will present both the GTK alert and read the alert out loud.
+func (p *PiPanelGTK) ShowAlert(e pipanel.AlertEvent) error {
+	err := p.GUI.ShowAlert(e)
+
+	if err != nil {
+		return err
+	}
+
+	return p.TTSAlerter.ShowAlert(e)
 }
