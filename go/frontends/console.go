da94f684468496f57ea402cfe04d028c5d06e445
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-01-01 16:26:29 -0500

    Update frontend factories to use struct.

--- go/frontends/console.go
+++ go/frontends/console.go
@@ -3,24 +3,20 @@ package frontends
 import (
 	"log"
 
+	pipanel "github.com/BenJetson/pipanel/go"
 	"github.com/BenJetson/pipanel/go/frontends/alerters/alertlog"
 	"github.com/BenJetson/pipanel/go/frontends/audio_players/audiolog"
 	"github.com/BenJetson/pipanel/go/frontends/display_managers/displaylog"
 	"github.com/BenJetson/pipanel/go/frontends/power_managers/powerlog"
 )
 
-type ConsoleFrontend struct {
-	*alertlog.AlertLog
-	*audiolog.AudioLog
-	*displaylog.DisplayLog
-	*powerlog.PowerLog
-}
-
-func NewConsoleFrontend(log *log.Logger) *ConsoleFrontend {
-	return &ConsoleFrontend{
-		AlertLog:   alertlog.New(log),
-		AudioLog:   audiolog.New(log),
-		DisplayLog: displaylog.New(log),
-		PowerLog:   powerlog.New(log),
+// NewConsoleFrontend creates a pipanel.Frontend that writes all events to the
+// provided logger instance.
+func NewConsoleFrontend(log *log.Logger) *pipanel.Frontend {
+	return &pipanel.Frontend{
+		Alerter:        alertlog.New(log),
+		AudioPlayer:    audiolog.New(log),
+		DisplayManager: displaylog.New(log),
+		PowerManager:   powerlog.New(log),
 	}
 }
--- go/frontends/pipanel_gtk.go
+++ go/frontends/pipanel_gtk.go
@@ -2,45 +2,22 @@ package frontends
 
 import (
 	"log"
-	"time"
 
 	pipanel "github.com/BenJetson/pipanel/go"
-	"github.com/BenJetson/pipanel/go/frontends/alerters/ttsalerter"
-	"github.com/faiface/beep/speaker"
 
-	"github.com/BenJetson/pipanel/go/frontends/alerters/gtkalerter"
+	"github.com/BenJetson/pipanel/go/frontends/alerters/gtkttsalerter"
 	"github.com/BenJetson/pipanel/go/frontends/audio_players/beeper"
 	"github.com/BenJetson/pipanel/go/frontends/display_managers/displaylog"
 	"github.com/BenJetson/pipanel/go/frontends/power_managers/powerlog"
 )
 
-type PiPanelGTK struct {
-	*beeper.Beeper
-	*gtkalerter.GUI
-	*ttsalerter.TTSAlerter
-	*displaylog.DisplayLog
-	*powerlog.PowerLog
-}
-
-func NewPiPanelGTK(log *log.Logger) *PiPanelGTK {
-	speaker.Init(beeper.SampleRate, beeper.SampleRate.N(time.Second/10))
-
-	return &PiPanelGTK{
-		GUI:        gtkalerter.New(log),
-		TTSAlerter: ttsalerter.New(log, "/tmp/alert-tts/", "en"),
-		Beeper:     beeper.New(log, "/tmp/pipanel-sounds/"),
-		DisplayLog: displaylog.New(log),
-		PowerLog:   powerlog.New(log),
+// NewPiPanelGTK creates a pipanel.Frontend that supports the RPi official
+// touch display, includes GTK/TTS alerts, and systemd power management.
+func NewPiPanelGTK(log *log.Logger) *pipanel.Frontend {
+	return &pipanel.Frontend{
+		Alerter:        gtkttsalerter.New(log, "/tmp/alert-tts/", "en"),
+		AudioPlayer:    beeper.New(log, "/tmp/pipanel-sounds/"),
+		DisplayManager: displaylog.New(log),
+		PowerManager:   powerlog.New(log),
 	}
-}
-
-// ShowAlert will present both the GTK alert and read the alert out loud.
-func (p *PiPanelGTK) ShowAlert(e pipanel.AlertEvent) error {
-	err := p.GUI.ShowAlert(e)
-
-	if err != nil {
-		return err
-	}
-
-	return p.TTSAlerter.ShowAlert(e)
 }
