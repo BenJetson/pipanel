d05088aaaca0f6715dc3c30dc569c11fd7abac04
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-01-02 22:37:13 -0500

    Remove underscore for consistency.

---
+++ go/cmd/launcher/launcher.go
@@ -0,0 +1,56 @@
+package launcher
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	pipanel "github.com/BenJetson/pipanel/go"
+	"github.com/BenJetson/pipanel/go/server"
+)
+
+func RunApplication(frontend *pipanel.Frontend) {
+	// Create log instances.
+	logServer := log.New(os.Stdout, "server ", log.LstdFlags)
+	logFrontend := log.New(os.Stdout, "frontend ", log.LstdFlags)
+
+	// Create signaling channels for concurrent operations.
+	interrupt := make(chan os.Signal, 1)
+	shutdown := make(chan struct{}, 1)
+
+	// Notify interrupt channel when a SIGINT is detected.
+	signal.Notify(interrupt, os.Interrupt)
+
+	// Initialize frontend.
+	if err := frontend.Init(); err != nil {
+		panic(err)
+	}
+
+	// Start the server.
+	server := server.New(logServer, 1035, frontend)
+
+	go server.ListenAndServe(shutdown)
+
+	// Create cleanup function for use upon interrupt/shutdown.
+	cleanup := func() {
+		if err := server.Shutdown(context.TODO()); err != nil {
+			panic(err)
+		}
+
+		if err := frontend.Cleanup(); err != nil {
+			panic(err)
+		}
+	}
+
+	// Wait for all goroutines to shut down.
+	select {
+	case <-interrupt:
+		fmt.Println("sigint detected")
+		cleanup()
+	case <-shutdown:
+		fmt.Println("server shutdown detected")
+		cleanup()
+	}
+}
--- go/frontends/pipanelgtk.go
+++ go/frontends/pipanelgtk.go
@@ -11,6 +11,10 @@ import (
 	"github.com/BenJetson/pipanel/go/frontends/power_managers/systemdpwr"
 )
 
+// The line below will cause a Makefile target, go build tag, and main frontend
+// factory to be generated for this type of frontend.
+// go:generate go run gen.go PiPanelGTK
+
 // NewPiPanelGTK creates a pipanel.Frontend that supports the RPi official
 // touch display, includes GTK/TTS alerts, and systemd power management.
 func NewPiPanelGTK(log *log.Logger) *pipanel.Frontend {
