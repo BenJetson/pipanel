9d2f3e213ad91ce5fbb56904e46cfbbf2590d6c5
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-01-02 22:50:05 -0500

    Add main for console frontend.

---
+++ go/cmd/console/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	"github.com/BenJetson/pipanel/go/cmd/launcher"
+	"github.com/BenJetson/pipanel/go/frontends"
+)
+
+func main() { launcher.RunApplication(frontends.NewConsoleFrontend()) }
