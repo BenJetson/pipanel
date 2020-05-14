998f1884148f92200c82542faa657b123bd78e02
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-05-14 18:43:59 -0400

    Load configuration into GTKAlerter. #9

--- go/frontends/alerters/gtkalerter/gui.go
+++ go/frontends/alerters/gtkalerter/gui.go
@@ -1,8 +1,11 @@
 package gtkalerter
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"log"
+	"time"
 
 	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
@@ -10,11 +13,69 @@ import (
 	pipanel "github.com/BenJetson/pipanel/go"
 )
 
+// Config specifies the options that modify the behavior of GTKAlerter.
+type Config struct {
+	// FontSize is the font size that should be used for the alert text content,
+	// measured in in points.
+	FontSize int `json:"font_size"`
+	// Defaults contains the values that will replace zero values on the
+	// incoming pipanel.AlertEvents.
+	Defaults struct {
+		// Timeout is the default timeout value, measured in seconds.
+		// If a TimeoutRange is specified, this value MUST be within that range.
+		Timeout int `json:"timeout"`
+		// Icon is the default icon value, which is the name of a gtk icon.
+		Icon string `json:"icon"`
+	} `json:"defaults"`
+	// TimeoutRange controls the range of values that are acceptable for
+	// the timeout field.
+	TimeoutRange struct {
+		// Min is the minimum timeout value. If this value is zero, the lower
+		// bound will not be checked.
+		Min time.Duration `json:"min"`
+		// Max is the maximum timeout value. If this value is zero, the upper
+		// bound will not be checked.
+		Max time.Duration `json:"max"`
+	} `json:"timeout_range"`
+	// ForbidPerpetual determines whether or not perpetual alerts are allowed.
+	// If this flag is true, the perpetual flag of incoming alerts will be
+	// ignored.
+	ForbidPerpetual bool `json:"forbid_perpetual"`
+}
+
+func validateConfig(log *log.Logger, cfg *Config) error {
+	if cfg.FontSize < 1 {
+		return errors.New("font size must be set to a value greater than zero")
+	}
+
+	if len(cfg.Defaults.Icon) < 1 {
+		return errors.New("default icon must be set")
+	}
+
+	if cfg.TimeoutRange.Min < 0 {
+		return errors.New("min timeout cannot be less than zero")
+	} else if cfg.TimeoutRange.Max < 0 {
+		return errors.New("max timeout cannot be less than zero")
+	} else if cfg.TimeoutRange.Min > cfg.TimeoutRange.Max {
+		return errors.New("min timeout cannot be greater than max timeout")
+	}
+
+	if cfg.Defaults.Timeout < 1 {
+		return errors.New("default timeout cannot be less than zero")
+	} else if cfg.Defaults.Timeout == 0 {
+		log.Println("WARNING: default timeout of zero may cause alerts " +
+			"without a timeout set to not display a window!")
+	}
+
+	return nil
+}
+
 // GUI is a GTK application that is capable of responding to PiPanel alert
 // events by showing them on the screen.
 type GUI struct {
 	log     *log.Logger
 	windows []*alertWindow
+	cfg     Config
 }
 
 // New creates a fresh GUI instance.
@@ -40,9 +101,22 @@ func (g *GUI) ShowAlert(e pipanel.AlertEvent) error {
 
 // Init initializes this GUI instance, setting the logger and starting the GTK
 // main event loop in a separate goroutine.
-func (g *GUI) Init(log *log.Logger, _ json.RawMessage) error {
+func (g *GUI) Init(log *log.Logger, rawCfg json.RawMessage) error {
 	g.log = log
 
+	// Decode the config.
+	d := json.NewDecoder(bytes.NewReader(rawCfg))
+	d.DisallowUnknownFields()
+
+	if err := d.Decode(&g.cfg); err != nil {
+		return err
+	}
+
+	if err := validateConfig(log, &g.cfg); err != nil {
+		return err
+	}
+
+	// Start the GTK main event loop.
 	gtk.Init(nil)
 	go gtk.Main()
 
