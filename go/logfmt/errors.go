de6749bad9611c3d434d2b5912f47a05b5847446
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-06-27 00:50:55 -0400

    Move error log annotation package. #22

--- go/logfmt/errors.go
+++ go/logfmt/errors.go
@@ -1,4 +1,4 @@
-package errlog
+package logfmt
 
 import (
 	"fmt"
@@ -7,20 +7,20 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const errKey = "error"
-const stackKey = "stack"
+const ErrKey = "error"
+const StackKey = "stack"
 
 // WithError returns a *logrus.Entry derived from the given *logrus.Entry, with
 // added fields for the error message and (if present) stack trace.
 //
 // Caller must ensure that err is not nil.
 func WithError(l *logrus.Entry, err error) *logrus.Entry {
-	f := logrus.Fields{errKey: err.Error()}
+	f := logrus.Fields{ErrKey: err.Error()}
 
 	if stackErr, ok := err.(interface {
 		StackTrace() errors.StackTrace
 	}); ok {
-		f[stackKey] = fmt.Sprintf("%+v", stackErr.StackTrace())
+		f[StackKey] = fmt.Sprintf("%+v", stackErr.StackTrace())
 	}
 
 	return l.WithFields(f)
